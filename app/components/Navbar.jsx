a5e3b60368a006fe67e6b52d4e67c625f1551cde
Vanessa-fe <van3ssacod3@gmail.com>
2024-04-18 11:21:34 +0200

    first commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "./yoda.png";
+export default function Navbar() {
+  return (
+    <nav>
+      <Image
+        src={Logo}
+        alt="Yoda Ticketing logo"
+        width={75}
+        placeholder="blur"
+        quality={100}
+      />
+
+      <h1>Fran&apos;s Next.js 13 Headless WP Example!!!</h1>
+      <Link href="/">Home</Link>
+      <Link href="/login">Login</Link>
+    </nav>
+  );
+}
